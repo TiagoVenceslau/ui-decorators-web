4f54377e93f72e5a1b1262af53b08e7ee166e676
Tiago <tiago@mail.com>
2021-12-14 02:40:55 +0000

    refs #1 - frist commit

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   verbose: true,
   transform: {'^.+\\.ts?$': 'ts-jest'},
   testEnvironment: 'node',
-  testRegex: '/tests/.*\\.(test|spec)\\.(ts|tsx)$',
+  testRegex: '/test.*\\.(test|spec)\\.(ts|tsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverage: true,
   coverageDirectory: "./workdocs/coverage",
