30d60303b20903ff295e0ae75b5825e39790332c
Tiago <Tiago@mail.com>
2021-12-16 18:41:10 +0000

    refs #1 - update

--- src/components/ionic-ui-input-element/test/ionic-ui-input-element.e2e.ts
+++ src/components/ionic-ui-input-element/test/ionic-ui-input-element.e2e.ts
@@ -19,7 +19,22 @@ describe('ionic-ui-input-element', () => {
   })
 
   it('renders', async () => {
-    await page.setContent('<ionic-ui-input-element input-id="id" input-name="id" input-prefix="input-" type="text" placeholder="place text here" label="This is a label" required="true" maxlength="15" minlength="5"\ pattern="^\w+$"></ionic-ui-input-element>');
+
+    const html = `
+    <ionic-ui-input-element input-id="id"
+                            input-name="id"
+                            input-prefix="input-"
+                            type="text"
+                            placeholder="place text here"
+                            label="This is a label"
+                            required="true"
+                            maxlength="15"
+                            minlength="5"\
+                            pattern="^\w+$"
+    ></ionic-ui-input-element>
+    `
+
+    await page.setContent(html);
 
     const element = await page.find('ionic-ui-input-element');
     expect(element).toHaveClass('hydrated');
